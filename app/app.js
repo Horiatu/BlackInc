b2f94d51c08504fbcb22a683dfba9b532f09319c
Horia Tudosie <htudosie@esri.ca>
2017-03-14 15:18:48 -0400

    Use directive toggle-class, toggle-class-on

--- app/app.js
+++ app/app.js
@@ -2,24 +2,24 @@
     'use strict';
 
 	angular
-		.module("blackInkApp", []);
+		.module("blackInkApp", [])
 
-		// .directive('toggleClass', function() { 
-		// 	return { 
-		// 		restrict: 'A', 
-		// 		link: function(scope, element, attrs) { 
-		// 			element.bind('click', function() { 
-		// 				if (attrs.toggleClassOn) { 
-		// 					var e = document.querySelectorAll(attrs.toggleClassOn); 
-		// 					angular.element(e).toggleClass(attrs.toggleClass); 
-		// 				} 
-		// 				else 
-		// 				{ 
-		// 					element.toggleClass(attrs.toggleClass); 
-		// 				} 
-		// 			}); 
-		// 		} 
-		// 	}; 
-		// });
+		.directive('toggleClass', function() { 
+			return { 
+				restrict: 'A', 
+				link: function(scope, element, attrs) { 
+					element.bind('click', function() { 
+						if (attrs.toggleClassOn) { 
+							var e = document.querySelectorAll(attrs.toggleClassOn); 
+							angular.element(e).toggleClass(attrs.toggleClass); 
+						} 
+						else 
+						{ 
+							element.toggleClass(attrs.toggleClass); 
+						} 
+					}); 
+				} 
+			}; 
+		});
 		
 })(); 
