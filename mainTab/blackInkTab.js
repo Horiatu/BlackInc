1761effbcf443cc6613bd6d3c8aeaded909e94c0
Horia Tudosie <htudosie@esri.ca>
2017-03-15 17:49:49 -0400

    cleanup

--- mainTab/blackInkTab.js
+++ mainTab/blackInkTab.js
@@ -1,18 +1,24 @@
 console.log('blackInkTab.js loaded');
 
-injectCss = function() {
-    if(!document.getElementById("blackInkCss")) {
-        this._injectCss('<link id="blackInkCss" rel="stylesheet" type="text/css" href="' + 
-            chrome.extension.getURL('/mainTab/blackInkTab.css') + '" />');
-    }
-};
+// injectCss = function() {
+//     if(!document.getElementById("blackInkCss")) {
+//         this._injectCss('<link id="blackInkCss" rel="stylesheet" type="text/css" href="' + 
+//             chrome.extension.getURL('/mainTab/blackInkTab.css') + '" />');
+//     }
+// };
 
-_injectCss = function(css) {
+injectCss = function(id, css) {
+	var element = document.getElementById(id);
+	if(element) {
+		element.parentNode.removeChild(element);
+	}
     if ($("head").length === 0) {
         $("body").before(css);
     } else {
-        $("head").before(css);
+        $("head").append(css);
     }
 };
 
-injectCss();
+// injectCss();
+
+ injectCss("BlackInkColor", '<style id="BlackIncColor" class="BlackInc">* {color:black !important;}</style>');
