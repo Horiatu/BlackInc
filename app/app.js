2f72cc323743510e5940d26a22e98fcc8b3756c2
Horia Tudosie <htudosie@esri.ca>
2017-09-06 16:29:12 -0400

    option page menu working well

--- app/app.js
+++ app/app.js
@@ -2,12 +2,27 @@
     'use strict';
 
 	angular.module("blackInkApp", ['ngAria'])
-	
+
 		.directive('myKey', function() {
 		    return {
 		        restrict: 'EA',
 		        replace: true,
 		        template: '-All that Html here-'
+		    };
+		})
+
+    	.directive('scroll', function ($window) {
+		    return function(scope, element, attrs) {
+		        angular.element($window).bind("scroll", function() {
+		            if (this.pageYOffset >= 100) {
+		                scope.boolChangeClass = true;
+		                console.log('Scrolled below header.');
+		            } else {
+		                scope.boolChangeClass = false;
+		                console.log('Header is in view.');
+		            }
+		            scope.$apply();
+		        });
 		    };
 		})
 
