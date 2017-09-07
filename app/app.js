8d6941103353f538dba977a14b25aec7c7212ab7
Horia Tudosie <htudosie@esri.ca>
2017-09-07 15:21:39 -0400

    crollIf, scroll

--- app/app.js
+++ app/app.js
@@ -30,7 +30,7 @@
   			return function (scope, element, attributes) {
     			setTimeout(function () {
       			if (scope.$eval(attributes.scrollIf)) {
-        		window.scrollTo(0, element[0].offsetTop - 100);
+        		window.scrollTo(0, element[0].offsetTop - 50);
 	      	}
     	});
     };
