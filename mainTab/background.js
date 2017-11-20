8ac5a2c4816f0278ad13733479418855c025da9b
Horia Tudosie <htudosie@esri.ca>
2017-11-20 14:37:58 -0500

    Added Analytics

--- mainTab/background.js
+++ mainTab/background.js
@@ -1,3 +1,16 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-109917224-3']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
+
 var manifest = chrome.runtime.getManifest();
 var thisVersion = manifest.version;
 this.versionMsg = manifest.name + " version <b>" + thisVersion + "</b>";
